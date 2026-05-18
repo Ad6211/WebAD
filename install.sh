cf1a84fa92aaac5c65bbe833b54239c69d0bea57
Ad6211 <adnojojo@gmail.com>
2026-05-18 21:23:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ APP_NAME="WebAD"
 
 INSTALL_DIR="$HOME/.local/bin/webad"
 APPIMAGE_PATH="$INSTALL_DIR/WebAD.AppImage"
-
 APPIMAGE_URL="https://github.com/Ad6211/WebAD/releases/download/v1.0/WebAD-x86_64.AppImage"
 
 echo "🚀 Installing $APP_NAME..."
