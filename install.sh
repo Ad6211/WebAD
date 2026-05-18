24f0b1ab975eb3c242a7cd47fdc834e9d7fb9c9d
Ad6211 <adnojojo@gmail.com>
2026-05-18 21:18:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
+APPIMAGE_PATH="$HOME/.local/bin/webad/WebAD.AppImage"
+
+mkdir -p "$HOME/.local/bin/webad"
+
 echo "⬇️ Downloading AppImage..."
 
 curl -L --fail --progress-bar \
   -o "$APPIMAGE_PATH" \
   "https://github.com/Ad6211/WebAD/releases/download/v1.0/WebAD-x86_64.AppImage"
-
-echo "✅ Download finished"
